e971246662157996b6ed4e293eb1e7ef9906f5cb
Malal0 <mj1055119@gmail.com>
2023-10-28 02:31:11 -0500

    updated

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Quizlet</h1>
+      <ul>
+        <li>import quiz data</li>
+        <li>make question block component</li>
+        <li>make question</li>
+        <li>make answer components</li>
+        <li>make css</li>
+        <li>make submit button</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
